70641674aca8f9e0ec8382a59fdc90ab4efc3db4
Fethi Chouk <choukfethi@outlook.com>
2022-05-08 15:31:43 -0900

    Jsximages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from "./images/imageInSrc.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div myStyle='myStyle'>
+
+<h1 className='title red'>Your name here</h1>
+
+<img src={image} alt="" />
+
+<img src="/imageInPublic.jpg" alt="" />
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
     </div>
   );
 }
